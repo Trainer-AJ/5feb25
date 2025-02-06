ae76093a29276c8e953a5046d616efd71342d6a8
git-of-aj <ananay.wfhintellify@gmail.com>
2025-02-06 14:22:19 +0530

    OMG !! By mistake Pushed secret !!!

---
+++ rg.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # Configuration options
+  client_id = "08527c67-b558XXXXXXXXXXXx"
+  client_secret   = "jbr8Q~guvvHXXXXXXXXXXXXXXXXXxn"
+  tenant_id       = "cb561bac-8XXXXXXXXXXXXXXXXX3ae"
+  subscription_id = "910fcc57-XXXXXXXXXXXXx"
+  use_oidc        = true
+}
+
+resource "azurerm_resource_group" "name" {
+
+  name     = "test"
+  location = "west us"
+  tags = {
+    new = "value",
+    env = "prod"
+  }
+}
