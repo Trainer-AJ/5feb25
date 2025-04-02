7e2effa67dc70ddddaa96c32a1731088d4289eda
trainer-aj <ananay.wfhintellify@gmail.com>
2025-04-02 19:56:09 +0530

    1

--- create-rg.sh
+++ create-rg.sh
@@ -4,3 +4,10 @@ read "Enter Location: "  loc
 read "Enter Name: " name
 
 az group create -n $name -l $loc
+
+name=mausi$RANDOM
+
+for i in {1..5}
+do 
+    az storage account create -n "${name}sa${i}" -g $name -l $loc --sku Standard_LRS --kind StorageV2
+done
